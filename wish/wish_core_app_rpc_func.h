bbee75d78300aa3877de55c82c17eed1fcc217c5
Jan Nyman <jan.nyman@controlthings.fi>
2017-02-15 11:25:16 +0200

    Change to core RPC identity.create or identity.remove
    When identity is created/removed, it becomes online for each local
    service on the core, and peer updates are sent to the services
    accordingly.

--- wish/wish_core_app_rpc_func.h
+++ wish/wish_core_app_rpc_func.h
@@ -2,6 +2,7 @@
 #define WISH_CORE_APP_RPC_FUNC_H
 
 #include "wish_service_registry.h"
+#include "wish_identity.h"
 
 void wish_core_app_rpc_init(void);
 
@@ -10,4 +11,7 @@ void wish_core_app_rpc_handle_req(uint8_t src_wsid[WISH_ID_LEN], uint8_t *data);
 void wish_core_app_rpc_cleanup_requests(uint8_t *wsid);
 
 void wish_send_peer_update_locals(uint8_t *dst_wsid, struct wish_service_entry *service_entry, bool online);
+
+void wish_report_identity_to_local_services(wish_identity_t* identity, bool online);
+
 #endif 
