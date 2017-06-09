304797b8d554ae3c328a61662648e79cca24ec35
André Kaustell <andre.kaustell@controlthings.fi>
2017-06-09 12:13:48 +0300

    Added arm cross compiling makefile.

--- port/unix/app_server.c
+++ port/unix/app_server.c
@@ -150,7 +150,7 @@ void send_core_to_app_via_tcp(wish_core_t* core, uint8_t wsid[WISH_ID_LEN], uint
 #endif
             
             if (write_ret != 2+len) {
-                printf("App connection: Write error! (c) Wanted %i got %li\n", 2, write_ret);
+                printf("App connection: Write error! (c) Wanted %i got %zu\n", 2, write_ret);
                 return;
             }
             return;
