69c3ab0d1ec81130cfb07fb4b0fe598e7f1620a8
André Kaustell <andre.kaustell@controlthings.fi>
2018-01-04 17:33:36 +0200

    Added Apache-2.0 license text file. Move source release file instead of copying when done.

--- tools/release-source-apache2.sh
+++ tools/release-source-apache2.sh
@@ -2,4 +2,4 @@ VERSION=`git describe`;
 npm i
 node node_modules/gulp/bin/gulp.js
 TARGET=../build/wish-${VERSION}-source.tar.gz
-cp ../build/wish-source.tar.gz $TARGET
+mv ../build/wish-source.tar.gz $TARGET
