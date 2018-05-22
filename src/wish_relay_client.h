a06edab9953700ea78af9bc0894a9159d44c620d
Jan Nyman <jan.nyman@controlthings.fi>
2018-05-22 09:44:06 +0300

    Allow re-definition of relay server without compiler warnings

--- src/wish_relay_client.h
+++ src/wish_relay_client.h
@@ -9,7 +9,9 @@
 #include "wish_utils.h"
 
 /* Define Relay server IP and port: */
+#ifndef RELAY_SERVER_HOST
 #define RELAY_SERVER_HOST "193.65.54.131:40000"
+#endif
 
 #define RELAY_SERVER_TIMEOUT 45 /* Seconds */
 
