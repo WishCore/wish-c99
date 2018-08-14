4cb51dd207a6ec753be0f6b08c04ab8f1f368373
André Kaustell <andre.kaustell@controlthings.fi>
2018-08-14 10:24:40 +0300

    Updated source-release script.

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -8,9 +8,10 @@ var sequence = require('gulp-sequence')
 var gitDescribeSync = require('git-describe').gitDescribeSync;
 
 var banner = ['/**',
-      ' * Wish - Peer-to-peer identity based network layer',
-      ' *',
-      ' * Copyright (C) 2018, ControlThings Oy Ab, All Rights Reserved',
+      ' * Copyright (C) 2018, ControlThings Oy Ab',
+      ' * Copyright (C) 2018, André Kaustell',
+      ' * Copyright (C) 2018, Jan Nyman',
+      ' * Copyright (C) 2018, Jepser Lökfors',
       ' *',
       ' * Licensed under the Apache License, Version 2.0 (the "License"); you may',
       ' * not use this file except in compliance with the License.',
